ca4c43ef26d41db02eacc4d83f5daf2cfcf752eb
Surajit <surajitmaity043@gmail.com>
2020-10-14 22:16:10 +0530

    cnange port

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path= require("path");
 const app = express();
 const fs =require("fs")
-const port = process.env.PORT || 80 ;
+const port = process.env.PORT || 8080 ;
 
 // EXPRESS SPECIFIC STUFF
 app.use("/static",express.static("static")); // this is the format to serve static Files
